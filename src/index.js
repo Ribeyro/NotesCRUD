3a39798eeb0c6509d55feae73c469f30363f4181
ribeyro <ribeyro.quispe@tecsup.edu.pe>
2024-11-06 12:08:53 -0500

    Cmabio port

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import "./database.js";
 
 async function main() {
   await createAdminUser();
-  app.listen(app.get("port"));
-
-  console.log("Server on port", app.get("port"));
-  console.log("Environment:", process.env.NODE_ENV);
+  app.listen(app.get("port"), '0.0.0.0', () => {
+    console.log("Server on port", app.get("port"));
+    console.log("Environment:", process.env.NODE_ENV);
+  });
 }
 
 main();
